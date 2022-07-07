873a79a5ea80bb5e07a1fa84cdbf6e4ec2c2d5c6
LoreMaru <gialma1120@hotmail.it>
2022-07-08 00:24:27 +0200

    feat: implementato metodo con oggetti

--- 07-07/script.js
+++ 07-07/script.js
@@ -1,4 +1,36 @@
-//Esercizio base + Bonus track fino al punto 3 (aggiungere btn per eliminare i task)
+// //Esercizio base + Bonus track fino al punto 3 (aggiungere btn per eliminare i task)
+
+// const taskInput = document.querySelector(".taskInput");
+
+// const inputBtn = document.querySelector(".inputBtn");
+
+// const taskList = document.querySelector(".taskList");
+
+// const deleteBtn = document.querySelector(".deleteBtn");
+
+// const taskTime = `${new Date().getHours()}:${new Date().getMinutes()}`;
+
+// function taskCreator() {
+//   taskList.innerHTML += `<li><input type="checkbox"><span>${taskInput.value}</span><br>
+//       <span>Task aggiunta alle ${taskTime} </span>
+//       <button class="deleteBtn">Elimina</button></li>`;
+// }
+
+// inputBtn.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   taskCreator();
+//   taskInput.value = "";
+// });
+
+// taskList.addEventListener("click", function (event) {
+//   if (event.target.className === "deleteBtn") {
+//     event.target.closest("li").remove();
+//   }
+// });
+
+//>>>>>>>>>>>>>>>>
+
+//Esercizio eseguito su oggetti. I task rimangono nella localStorage ma non vengono eliminati dal tasto
 
 const taskInput = document.querySelector(".taskInput");
 
@@ -8,17 +40,47 @@ const taskList = document.querySelector(".taskList");
 
 const deleteBtn = document.querySelector(".deleteBtn");
 
-const taskTime = `${new Date().getHours()}:${new Date().getMinutes()}`;
+//Stato pagina al refresh
 
-function taskCreator() {
-  taskList.innerHTML += `<li><input type="checkbox"><span>${taskInput.value}</span><br>
-      <span>Task aggiunta alle ${taskTime} </span>
-      <button class="deleteBtn">Elimina</button></li>`;
+for (const [key, value] of Object.entries(localStorage).sort()) {
+  taskList.innerHTML += `<li><input type="checkbox">
+  <span>${value}</span><br>
+  <button class="deleteBtn">Elimina</button></li>`;
 }
+
+// oggetto tast
+const task = {
+  _taskContent: [],
+
+  get taskText() {
+    return this._taskContent;
+  },
+  set taskText(value) {
+    this._taskContent = value;
+    localStorage.setItem(
+      `${new Date().toLocaleTimeString()}`,
+      `${this._taskContent}`
+    );
+  },
+
+  render() {
+    taskList.innerHTML += `<li><input type="checkbox">
+      <span>${this._taskContent}</span><br>
+      <button class="deleteBtn">Elimina</button></li>`;
+  },
+};
 
 inputBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  taskCreator();
+
+  let date = new Date();
+  let day = date.toDateString();
+  let time = date.toLocaleTimeString();
+  let milliseconds = date.getMilliseconds();
+  let completeDate = `${day} ${time}`;
+
+  task.taskText = `${taskInput.value} <br><br> ${completeDate}`;
+  task.render();
   taskInput.value = "";
 });
 
